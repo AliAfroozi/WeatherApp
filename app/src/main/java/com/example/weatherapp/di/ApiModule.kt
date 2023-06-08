b3be531e0a6892b63971583000b5f9942b7fb33b
AliAfroozi <ali.afroozi.dev@gmail.com>
2023-06-08 23:11:03 +0330

    DEV: implement api interface and config thats properties in ApiModule.
     Description: set base url, httpClient and other properties of api instance and provide it using hilt in api module.

--- app/src/main/java/com/example/weatherapp/di/ApiModule.kt
+++ app/src/main/java/com/example/weatherapp/di/ApiModule.kt
@@ -1,4 +1,45 @@
 package com.example.weatherapp.di
 
+import com.example.weatherapp.feature_show_weather.data.data_source.WeatherApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
 object ApiModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit {
+
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val httpClient = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+
+        return Retrofit.Builder()
+            .baseUrl("https://api.weatherapi.com")
+            .client(httpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideWeatherApi(): WeatherApi {
+        return provideRetrofit().create(WeatherApi::class.java)
+    }
+
+
 }
--- app/src/main/java/com/example/weatherapp/feature_show_weather/data/data_source/WeatherApi.kt
+++ app/src/main/java/com/example/weatherapp/feature_show_weather/data/data_source/WeatherApi.kt
@@ -1,4 +1,13 @@
 package com.example.weatherapp.feature_show_weather.data.data_source
 
+import com.example.weatherapp.feature_show_weather.domain.model.SelectedLocationWeather
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface WeatherApi {
+
+    @GET("/v1/current.json")
+    suspend fun getCurrentLocationWeather(@Query("key") key: String , @Query("q") queryParam: String , @Query("api") api : String = "yes") : Response<SelectedLocationWeather>
+
 }
