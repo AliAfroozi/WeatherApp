fe3d3239601a7d29026c03f00a9b84a7e25371e8
AliAfroozi <ali.afroozi.dev@gmail.com>
2023-06-09 01:21:05 +0330

    REFACTOR: refactor WeatherRepositoryImpl with attention to WeatherRepository changes in domain layer.

--- app/src/main/java/com/example/weatherapp/feature_show_weather/data/repository/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/example/weatherapp/feature_show_weather/data/repository/WeatherRepositoryImpl.kt
@@ -1,10 +1,22 @@
 package com.example.weatherapp.feature_show_weather.data.repository
 
+import com.example.weatherapp.feature_show_weather.data.data_source.WeatherApi
 import com.example.weatherapp.feature_show_weather.domain.model.SelectedLocation
+import com.example.weatherapp.feature_show_weather.domain.model.SelectedLocationWeather
 import com.example.weatherapp.feature_show_weather.domain.repository.WeatherRepository
+import com.example.weatherapp.feature_show_weather.presentation.MainActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import javax.inject.Inject
 
-class WeatherRepositoryImpl: WeatherRepository {
-    override fun getCityWeather(selectedLocation: SelectedLocation) {
-        TODO("Not yet implemented")
+class WeatherRepositoryImpl @Inject constructor(private val weatherApi: WeatherApi) :
+    WeatherRepository {
+    override suspend fun getPointWeather(selectedLocation: SelectedLocation): Response<SelectedLocationWeather> {
+        val q = selectedLocation.lat.toString() + "," + selectedLocation.long.toString()
+        return weatherApi.getCurrentLocationWeather(
+            key = MainActivity.API_KEY,
+            queryParam = q)
+
     }
 }
