e9bf2acebecb678b117c5b295d93977c4125b342
AliAfroozi <ali.afroozi.dev@gmail.com>
2023-06-09 01:19:47 +0330

    DEV: implement Domain layer of app.
     Description: compelete weatherRepo in domain layer by makeing it suspend, change return type. compelete LocationWeatherUseCase.

--- app/src/main/java/com/example/weatherapp/feature_show_weather/domain/repository/WeatherRepository.kt
+++ app/src/main/java/com/example/weatherapp/feature_show_weather/domain/repository/WeatherRepository.kt
@@ -1,7 +1,9 @@
 package com.example.weatherapp.feature_show_weather.domain.repository
 
 import com.example.weatherapp.feature_show_weather.domain.model.SelectedLocation
+import com.example.weatherapp.feature_show_weather.domain.model.SelectedLocationWeather
+import retrofit2.Response
 
 interface WeatherRepository {
-    fun getCityWeather(selectedLocation: SelectedLocation)
+    suspend fun getPointWeather(selectedLocation: SelectedLocation) : Response<SelectedLocationWeather>
 }
--- app/src/main/java/com/example/weatherapp/feature_show_weather/domain/use_case/LocationWeatherUseCase.kt
+++ app/src/main/java/com/example/weatherapp/feature_show_weather/domain/use_case/LocationWeatherUseCase.kt
@@ -1,4 +1,13 @@
 package com.example.weatherapp.feature_show_weather.domain.use_case
 
-class LocationWeatherUseCase {
+import com.example.weatherapp.feature_show_weather.domain.model.SelectedLocation
+import com.example.weatherapp.feature_show_weather.domain.model.SelectedLocationWeather
+import com.example.weatherapp.feature_show_weather.domain.repository.WeatherRepository
+import retrofit2.Response
+import javax.inject.Inject
+
+class LocationWeatherUseCase @Inject constructor(private val weatherRepository: WeatherRepository) {
+    suspend fun getLocationWeather(selectedLocation: SelectedLocation): Response<SelectedLocationWeather> {
+        return weatherRepository.getPointWeather(selectedLocation)
+    }
 }
