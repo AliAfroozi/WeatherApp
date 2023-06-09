93c61ef2b925ac57f1e657eff10e6cc88802f45a
AliAfroozi <ali.afroozi.dev@gmail.com>
2023-06-09 22:29:36 +0330

    Big and Werid Fail: try to fix this bug " multiple api calls in recomposition."
     Decription: in 8a87ba570b2b2f0d71e4820525a942232b0b5c78 commit, I add a issue report to github in which: in weather screen mutiple api calls happens with recomposition. I did a lot to solve that but see no success.

--- app/src/main/java/com/example/weatherapp/feature_show_weather/presentation/WeatherScreen.kt
+++ app/src/main/java/com/example/weatherapp/feature_show_weather/presentation/WeatherScreen.kt
@@ -1,42 +1,66 @@
 package com.example.weatherapp.feature_show_weather.presentation
 
+import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example.weatherapp.feature_show_weather.domain.model.SelectedLocation
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import com.example.weatherapp.feature_show_weather.domain.model.SelectedLocationWeather
+import kotlinx.coroutines.*
 
 @Composable
-fun WeatherScreen(lat: Double?, long: Double?, navController: NavHostController, weatherViewModel: WeatherViewModel = hiltViewModel()) {
+fun WeatherScreen(
+    lat: Double?,
+    long: Double?,
+    navHostController: NavHostController,
+    weatherViewModel: WeatherViewModel = hiltViewModel()
+) {
 
 
-    var weather = remember {
+    val weather = remember {
         mutableStateOf(weatherViewModel.weather)
     }
 
 
-    Box(modifier = Modifier.fillMaxSize() , contentAlignment = Alignment.Center) {
-        if (weather.value.value.isNotEmpty())
-            Text(text = weather.value.value[0].current.condition.text)
-        else {
-            if (long != null && lat != null && weather.value.value.isEmpty()) {
-                LaunchedEffect(Unit){
-                    CoroutineScope(Dispatchers.IO).launch {
-                        weatherViewModel.showPointWeather(SelectedLocation(lat , long))
-                    }
+
+    Log.e("ScreenShow ", "ScreenShow")
+
+
+
+
+    if (weatherViewModel.weather.isEmpty()){
+
+        LaunchedEffect(Unit) {
+            Log.e("API ", "Called API")
+            weatherViewModel.showPointWeather(SelectedLocation(lat!!, long!!)) {
+                if (it.isSuccessful){
+                    weatherViewModel.weather = listOf(it.body()!!)
                 }
             }
+
+        }
+    }
+
+
+
+    if (weatherViewModel.weather.isEmpty()) {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            CircularProgressIndicator()
+        }
+    }
+
+    if (weatherViewModel.weather.isNotEmpty()) {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Text(text = weatherViewModel.weather[0].current.condition.text)
         }
     }
 }
+
--- app/src/main/java/com/example/weatherapp/feature_show_weather/presentation/WeatherViewModel.kt
+++ app/src/main/java/com/example/weatherapp/feature_show_weather/presentation/WeatherViewModel.kt
@@ -1,29 +1,40 @@
 package com.example.weatherapp.feature_show_weather.presentation
 
 import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.weatherapp.feature_show_weather.data.data_source.WeatherApi
+import com.example.weatherapp.feature_show_weather.data.repository.WeatherRepositoryImpl
 import com.example.weatherapp.feature_show_weather.domain.model.SelectedLocation
 import com.example.weatherapp.feature_show_weather.domain.model.SelectedLocationWeather
 import com.example.weatherapp.feature_show_weather.domain.use_case.LocationWeatherUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import retrofit2.Response
 import javax.inject.Inject
 
 @HiltViewModel
 class WeatherViewModel @Inject constructor(private val weatherUseCase: LocationWeatherUseCase) :
     ViewModel() {
 
-    var weather = mutableStateOf(listOf<SelectedLocationWeather>())
+     var weather = listOf<SelectedLocationWeather>()
 
-    init {
-    }
 
-    suspend fun showPointWeather(
+     fun showPointWeather(
         selectedLocation: SelectedLocation,
+        onResponse: (Response<SelectedLocationWeather>) -> Unit
     ) {
-        if (weather.value.isNotEmpty())
-            weather.value = listOf(weatherUseCase.getLocationWeather(selectedLocation).body()!!)
+        viewModelScope.launch {
+            onResponse(weatherUseCase.getLocationWeather(selectedLocation))
+        }
     }
 
 }
+
+
+
+
